7b8669957baff33f1dff0d4ce4865f41936ad832
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-06-24 10:30:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+function moveDodgerLeft() {
+  var dodger = document.getElementById('dodger');
+  dodger.style.left = "160px"
+}
+
+function moveDodgerRight() {
+  var dodger = document.getElementById('dodger');
+  dodger.style.left = "200px"
+}
